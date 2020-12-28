3a5acf24cc897901b7276f986e7078635090618d
Dmitry Romanoff <dmitryr@jfrog.com>
2020-12-28 23:00:33 +0200

    Do some order

--- sql/check_queries_pg_stat_activity_100.sql
+++
@@ -1,2 +0,0 @@
-
-select datname, substr(query, 1, 150) the_query, (now() - pg_stat_activity.query_start) takes_time, state, client_addr, pid from pg_stat_activity  order by 3 desc;
--- sql/pg_stat_activity_queries.sql
+++ sql/pg_stat_activity_queries.sql
@@ -1,3 +1,5 @@
+select datname, substr(query, 1, 150) the_query, (now() - pg_stat_activity.query_start) takes_time, state, client_addr, pid from pg_stat_activity  order by 3 desc;
+
 select 
   datname, 
   substr(query, 1, 150) the_query, 
@@ -6,3 +8,4 @@ from
   pg_stat_activity 
 group by datname, substr(query, 1, 150)  
 order by 3 desc;
+
