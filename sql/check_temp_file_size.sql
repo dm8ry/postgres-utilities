1e69c882255d1171b7120b2106ae7266c9faaa92
Dmitry Romanoff <dmitry.romanoff@outlook.com>
2020-12-28 22:17:56 +0200

    Update check_temp_file_size.sql

--- sql/check_temp_file_size.sql
+++ sql/check_temp_file_size.sql
@@ -1,5 +1,22 @@
-SELECT datname, temp_files AS "Temporary files",temp_bytes AS "Size of temporary files" FROM pg_stat_database ;
+SELECT 
+  datname, 
+  temp_files AS "Temporary files",
+  temp_bytes AS "Size of temporary files" 
+FROM pg_stat_database;
 
-SELECT datname,temp_files AS "Temporary files"     , pg_size_pretty(temp_bytes) AS "Size of temporary files",stats_reset    FROM   pg_stat_database db order by temp_bytes desc; 
+SELECT 
+  datname,
+  temp_files AS "Temporary files", 
+  pg_size_pretty(temp_bytes) AS "Size of temporary files",
+  stats_reset    
+FROM pg_stat_database db 
+order by temp_bytes desc;
 
-select datname,  pg_size_pretty(pg_database_size(datname)) db_size, temp_files, pg_size_pretty(temp_bytes) temp_size, stats_reset from pg_stat_database order by temp_bytes desc
+select 
+  datname,  
+  pg_size_pretty(pg_database_size(datname)) db_size, 
+  temp_files, 
+  pg_size_pretty(temp_bytes) temp_size, 
+  stats_reset 
+from pg_stat_database 
+order by temp_bytes desc;
