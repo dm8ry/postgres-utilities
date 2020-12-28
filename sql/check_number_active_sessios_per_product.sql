522a0c7da7452bd659515b0268a23475c074a760
Dmitry Romanoff <dmitry.romanoff@outlook.com>
2020-12-28 22:10:46 +0200

    Update check_number_active_sessios_per_product.sql

--- sql/check_number_active_sessios_per_product.sql
+++ sql/check_number_active_sessios_per_product.sql
@@ -1,1 +1,10 @@
-select substr(datname, position('_' in datname)+1, 100) customer_type, count(1) n_of_active_sessions_per_product from pg_stat_activity where state!='idle' group by substr(datname, position('_' in datname)+1, 100) order by 2 desc;
+select 
+  substr(datname, position('_' in datname)+1, 100) customer_type, 
+  count(1) n_of_active_sessions_per_product 
+from 
+  pg_stat_activity 
+where 
+  state!='idle' 
+group by 
+  substr(datname, position('_' in datname)+1, 100) 
+order by 2 desc;
