109dc04cea18087d271906a561d4a0e524d85078
Dmitry <dmitry.romanoff@outlook.com>
2020-12-28 22:44:44 +0200

    Update pg_stat_activity_queries.sql

--- sql/pg_stat_activity_queries.sql
+++ sql/pg_stat_activity_queries.sql
@@ -1,1 +1,8 @@
-select datname, substr(query, 1, 150) the_query, count(1) from pg_stat_activity group by datname, substr(query, 1, 150)  order by 3 desc;
+select 
+  datname, 
+  substr(query, 1, 150) the_query, 
+  count(1) 
+from 
+  pg_stat_activity 
+group by datname, substr(query, 1, 150)  
+order by 3 desc;
