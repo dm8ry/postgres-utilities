42106e1ccd6353bcd07d6092fb911fbb4968fb3d
Dmitry Romanoff <dmitry.romanoff@outlook.com>
2020-12-28 22:12:34 +0200

    Update list_dbs_and_their_size.sql

--- sql/list_dbs_and_their_size.sql
+++ sql/list_dbs_and_their_size.sql
@@ -1,1 +1,8 @@
-SELECT pg_database.datname as "database_name", pg_database_size(pg_database.datname)/1024/1024 AS size_in_mb FROM pg_database where pg_database.datname like '%_xray' ORDER by 2 DESC;
+SELECT 
+  pg_database.datname as "database_name", 
+  pg_database_size(pg_database.datname)/1024/1024 AS size_in_mb 
+FROM 
+ pg_database 
+WHERE 
+  pg_database.datname like '%_xray' 
+ORDER by 2 DESC;
